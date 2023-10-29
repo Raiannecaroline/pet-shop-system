007267dbd6cab2e09064a2f538d8d3d35357ab9d
Raiane Caroline S Teixeira <raianecaroline.st@gmail.com>
2023-10-29 01:41:03 -0300

    update: Adicionando função de calculo

--- pet-shop.py
+++ pet-shop.py
@@ -1,6 +1,9 @@
+# Oi fofs, uma explicação breve sobre o sistema, bebi altas cervejas para fazer ele
+
 # O sistema do pet shop basea-se na venda de produtos para animais de estimação.
 # O sistema deve permitir o cadastro de produtos e clientes, além de permitir a venda de produtos.
 # O sistema vai pegar os dados de um json, guardar dados e listar estes dados pelo terminal.
+# O sistema vai pegar os dados Json e manipular os dados, como adicionar, remover, atualizar e pesquisar.
 
 import json
 import os
@@ -14,52 +17,40 @@ clientes = []
 # Lista para armazenar os pedidos
 pedidos = []
 
-valorTotalPedido = 0.0
-
 
 # Funções da parte dos produtos
 def adicionarProdutos(nome, quantidade, preco):
     produto = {"nome": nome, "quantidade": quantidade, "preco": preco}
     produtos[nome] = produto
-    adicionarProdutoAoJSON(produto)
+    salvarDadosJson()
 
 
 def removerProdutos(nome):
     produtos.pop(nome)
-    atualizarProdutoNoJSON()
+    salvarDadosJson()
 
 
 def atualizarProdutos(nome, quantidade):
     produtos[nome]["quantidade"] = quantidade
-    atualizarProdutoNoJSON()
+    salvarDadosJson()
 
 
 def pesquisarProdutos(nome):
     return produtos.get(nome)
 
 
-def adicionarProdutoAoJSON(produtos):
-    produto_json = {"produtos": list(produtos.values())}
-    with open("produtos.json", "w") as file:
-        json.dump(produto_json, file)
-
-
-def atualizarProdutoNoJSON():
-    adicionarProdutoAoJSON()
-
-
 # Funções da parte dos clientes
 def adicionarClientes(nome, cpf, email, telefone):
     cliente = {"nome": nome, "cpf": cpf, "email": email, "telefone": telefone}
     clientes.append(cliente)
-    adicionarProdutoAoJSON(cliente)
+    salvarDadosJson()
 
 
 def removerClientes(nome):
     for cliente in clientes:
         if cliente["nome"] == nome:
             clientes.remove(cliente)
-            atualizarClienteNoJSON()
+            salvarDadosJson()
             break
 
 
@@ -69,7 +60,7 @@ def atualizarClientes(nome, cpf, email, telefone):
             cliente["cpf"] = cpf
             cliente["email"] = email
             cliente["telefone"] = telefone
-            atualizarClienteNoJSON()
+            salvarDadosJson()
             break
 
 
@@ -79,17 +70,8 @@ def pesquisarClientes(nome):
             return cliente
 
 
-def adicionarClienteAoJSON(clientes):
-    cliente_json = {"clientes": clientes}
-    with open("clientes.json", "w") as file:
-        json.dump(cliente_json, file)
-
-
-def atualizarClienteNoJSON():
-    adicionarClienteAoJSON()
-
 # Funções da parte dos pedidos
-def adicionarPedidos(idPedido, cliente, produto, quantidade, valorTotalPedido):
+def adicionarPedidos(idPedido, cliente, produto, quantidade):
     pedido = {
         "id": idPedido,
         "cliente": cliente,
@@ -97,14 +79,15 @@ def adicionarPedidos(idPedido, cliente, produto, quantidade, valorTotalPedido):
         "quantidade": quantidade,
     }
     pedidos.append(pedido)
-    adicionarProdutoAoJSON(pedido)
+    salvarDadosJson()
     infoProdutos = pesquisarProdutos(produto)
     if infoProdutos:
         valorPedidos = infoProdutos["preco"] * quantidade
         valorTotalPedido += valorPedidos
+    return valorTotalPedido
 
 
-def removerPedido(idPedido, valorTotalPedido):
+def removerPedido(idPedido):
     for pedido in pedidos:
         if pedido["id"] == idPedido:
             infoProdutos = pesquisarProdutos(pedido["produto"])
@@ -112,13 +95,14 @@ def removerPedido(idPedido, valorTotalPedido):
                 valorPedidos = infoProdutos["preco"] * pedido["quantidade"]
                 valorTotalPedido -= valorPedidos
             pedidos.remove(pedido)
-            break
+            salvarDadosJson()
+        return valorTotalPedido
 
 
-def atualizarPedido(idPedido, cliente, produto, quantidade, valorTotalPedido):
+def atualizarPedido(idPedido, cliente, produto, quantidade):
     for pedido in pedidos:
         if pedido["id"] == idPedido:
-            infoProdutos = pesquisarProdutos(pedido["produto"])
+            infoProdutos = pesquisarProdutos(produto)
             if infoProdutos:
                 valorPedidosAntigos = infoProdutos["preco"] * pedido["quantidade"]
                 valorProdutosNovos = infoProdutos["preco"] * quantidade
@@ -127,7 +111,9 @@ def atualizarPedido(idPedido, cliente, produto, quantidade, valorTotalPedido):
                     pedido["cliente"] = cliente
                     pedido["produto"] = produto
                     pedido["quantidade"] = quantidade
-                    break
+                    salvarDadosJson()
+                    return valorTotalPedido
+    return valorTotalPedido
 
 
 def pesquisarPedido(idPedido):
@@ -135,65 +121,36 @@ def pesquisarPedido(idPedido):
         if pedido["id"] == idPedido:
             return pedido
 
-
-def adicionarPedidoAoJSON(pedidos):
-    pedido_json = {"pedidos": pedidos}
-    with open("pedidos.json", "w") as file:
-        json.dump(pedido_json, file)
-
-
-def atualizarPedidoNoJSON():
-    pedido_json = {"pedidos": pedidos}
-    with open("pedidos.json", "w") as file:
-        json.dump(pedido_json, file)
+ # Função para calcular o valor total dos pedidos       
+def calcularValorTotalPedido(pedidos):
+    total = 0
+    for pedido in pedidos:
+        infoProdutos = pesquisarProdutos(pedido["produto"])
+        if infoProdutos:
+            total += infoProdutos["preco"] * pedido["quantidade"]
+    return total                
 
 
 # Função para carregar os dados de um arquivo json
 def carregarDadoJson():
-    with open("produtos.json", "r") as file:
-        produto_json = json.load(file)
-
+    if os.path.exists("produtos.json"):
+        with open("produtos.json", "r") as file:
+            produto_json = json.load(file)
+        produtos.clear()
         for produto in produto_json["produtos"]:
-          adicionarProdutos(produto["nome"], produto["quantidade"], produto["preco"])
+            produtos[produto["nome"]] = produto
 
-    with open("clientes.json", "r") as file:
-        cliente_json = json.load(file)
+    if os.path.exists("clientes.json"):
+        with open("clientes.json", "r") as file:
+            cliente_json = json.load(file)
+        clientes.clear()
+        clientes.extend(cliente_json["clientes"])
 
-        for cliente in cliente_json["clientes"]:
-            adicionarClientes(
-                cliente["nome"], cliente["cpf"], cliente["email"], cliente["telefone"]
-            )
-
-    with open("pedidos.json", "r") as file:
-        pedido_json = json.load(file)
-
-        for pedido in pedido_json["pedidos"]:
-            adicionarPedidos(
-                pedido["id"],
-                pedido["cliente"],
-                pedido["produto"],
-                pedido["quantidade"],
-                valorTotalPedido,
-            )
-
-
-# Função para pesquisar produtos e clientes
-def pesquisarProduto():
-    nomeProduto = input("Digite o nome do produto desejado: ")
-    produto = pesquisarProdutos(nomeProduto)
-    if produto:
-        print(produto)
-    else:
-        print("Produto não encontrado")
-
-
-def pesquisarCliente():
-    nomeCliente = input("Digite o nome do cliente desejado: ")
-    cliente = pesquisarClientes(nomeCliente)
-    if cliente:
-        print(cliente)
-    else:
-        print("Cliente não encontrado")
+    if os.path.exists("pedidos.json"):
+        with open("pedidos.json", "r") as file:
+            pedido_json = json.load(file)
+        pedidos.clear()
+        pedidos.extend(pedido_json["pedidos"])
 
 
 # Função para salvar os dados em um arquivo json
@@ -201,21 +158,19 @@ def salvarDadosJson():
     produto_json = {"produtos": list(produtos.values())}
     with open("produtos.json", "w") as file:
         json.dump(produto_json, file)
-        adicionarClienteAoJSON(produto_json)
 
     cliente_json = {"clientes": clientes}
     with open("clientes.json", "w") as file:
         json.dump(cliente_json, file)
-        adicionarClienteAoJSON(cliente_json)
 
     pedido_json = {"pedidos": pedidos}
     with open("pedidos.json", "w") as file:
         json.dump(pedido_json, file)
-        adicionarClienteAoJSON(pedido_json)
 
 
 # Função para mostrar o menu
 def menu():
+    valorTotalPedido = 0.0
     while True:
         print("*** Bem vindo ao Pet Shop ***")
         print("1 - Listar os Produtos")
@@ -239,21 +194,21 @@ def menu():
                 print(produto)
 
         elif opcao == 2:
-            pesquisarProduto()
+            pesquisarProdutos()
 
         elif opcao == 3:
             for cliente in clientes:
                 print(cliente)
 
         elif opcao == 4:
-            pesquisarCliente()
+            pesquisarClientes()
 
         elif opcao == 5:
             for pedido in pedidos:
                 print(pedido)
 
         elif opcao == 6:
-            print("O valor total dos pedidos é {:.2f}: ".format(valorTotalPedido))
+            print("O valor total dos pedidos é {:.2f}".format(calcularValorTotalPedido(pedidos)))
 
         elif opcao == 7:
             nome = input("Digite o nome do produto: ")
@@ -289,16 +244,16 @@ def menu():
             cliente = input("Digite o nome do cliente: ")
             produto = input("Digite o nome do produto: ")
             quantidade = int(input("Digite a quantidade do produto: "))
-            adicionarPedidos(idPedido, cliente, produto, quantidade, valorTotalPedido)
-            print("Pedido adicionado com sucesso")      
+            adicionarPedidos(idPedido, cliente, produto, quantidade)
+            print("Pedido adicionado com sucesso")
 
         elif opcao == 12:
             idPedido = int(input("Digite o id do pedido: "))
             cliente = input("Digite o nome do cliente: ")
             produto = input("Digite o nome do produto: ")
             quantidade = int(input("Digite a quantidade do produto: "))
-            atualizarPedido(idPedido, cliente, produto, quantidade, valorTotalPedido)
-            print("Pedido atualizado com sucesso")      
+            atualizarPedido(idPedido, cliente, produto, quantidade)
+            print("Pedido atualizado com sucesso")
 
         elif opcao == 13:
             salvarDadosJson()
